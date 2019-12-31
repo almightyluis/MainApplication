732484c57f8d364dae6315284c8050f056c72091
Luis Gonzalez <otfgonzalez@gmail.com>
2019-12-31 11:05:07 -0800

    Initial Commit

---
+++ MainApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainApplication
+//
+//  Created by Luis Gonzalez on 12/31/19.
+//  Copyright © 2019 Luis Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
